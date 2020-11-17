1930e74eed3ea6c2640941856ffa51f3017c1141
JungTag <dbsdudxor121@naver.com>
2020-11-17 14:32:02 +0900

    11/17

---
+++ sal/src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+    return (
+        <>
+            <p>text1</p>
+            <p>text2</p>
+            <input type="file" name="file" id="" />
+        </>
+    );
+}
+
+export default App;
